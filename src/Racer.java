f593ea1bb239c494c3a2fc80d1e3af224dd1dfaa
ShawnStrong <shawnpstrong@gmail.com>
2017-03-25 16:22:28 -0500

    Update Racer.java

--- src/Racer.java
+++ src/Racer.java
@@ -1,19 +1,14 @@
 public class Racer {
 	public int racerNum;
-	public int totalRacers;
 	public long start;
 	public long fin;
 	public int index;
-	public int x;
-	public int y;
+
 	
 	public Racer (int rNum, int channel) {
 		
 		this.racerNum = rNum;
-		
-		// stores the channel to be turned on
-		this.x = totalRacers * 2 - 1;
-		this.y = totalRacers * 2;
+
 		this.start = 0;
 		this.fin = 0;
 		this.index = channel;
